99d9aad2c09e0cfc771417774744d78dcaf30ce0
Điền Nguyễn <duydien992006@gmail.com>
2026-09-23 09:37:41 +0700

    phần tiếp

---
+++ API/Controllers/BaoCaoController.cs
@@ -0,0 +1,25 @@
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "QuanTriVien")]
+    public class BaoCaoController : Controller
+    {
+        private readonly IBaoCaoBusiness _business;
+        public BaoCaoController(IBaoCaoBusiness business)
+        {
+            _business = business;
+        }
+
+        [HttpGet("tong-quan")]
+        public async Task<IActionResult> TongQuan()
+        {
+            var bc = await _business.LayTongQuanAsync();
+            return Ok(bc);
+        }
+    }
+}
---
+++ API/Controllers/ThongBaoController.cs
@@ -0,0 +1,35 @@
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ThongBaoController : Controller
+    {
+        private readonly IThongBaoBusiness _business;
+        public ThongBaoController(IThongBaoBusiness business)
+        {
+            _business = business;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LayCuaToi()
+        {
+            var claim = User.Claims.FirstOrDefault(c => c.Type == "Id");
+            int nguoiDungId = claim != null ? int.Parse(claim.Value) : 0;
+            var ds = await _business.LayTheoNguoiDungAsync(nguoiDungId);
+            return Ok(ds);
+        }
+
+        [HttpPut("{id:int}/da-doc")]
+        public async Task<IActionResult> DanhDauDaDoc(int id)
+        {
+            var kq = await _business.DanhDauDaDocAsync(id);
+            if (kq.ThanhCong) return Ok(kq);
+            return NotFound(kq);
+        }
+    }
+}

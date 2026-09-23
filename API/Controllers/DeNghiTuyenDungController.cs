c63d4ec30bb2a16e339e1c067a580666169cfd9c
Điền Nguyễn <duydien992006@gmail.com>
2026-09-23 09:33:44 +0700

    phần tiếp

--- API/Controllers/DeNghiTuyenDungController.cs
+++ API/Controllers/DeNghiTuyenDungController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Model;
 
 namespace API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class DeNghiTuyenDungController : Controller
     {
-        public IActionResult Index()
+        private readonly IDeNghiTuyenDungBusiness _business;
+        public DeNghiTuyenDungController(IDeNghiTuyenDungBusiness business)
         {
-            return View();
+            _business = business;
+        }
+
+        [HttpPost("gui")]
+        [Authorize(Roles = "NhaTuyenDung")]
+        public async Task<IActionResult> Gui([FromBody] DeNghiTuyenDungModel model)
+        {
+            var kq = await _business.GuiAsync(model);
+            if (kq.ThanhCong) return Ok(kq);
+            return BadRequest(kq);
+        }
+
+        [HttpPut("{id:int}/phan-hoi")]
+        [Authorize(Roles = "UngVien")]
+        public async Task<IActionResult> PhanHoi(int id, [FromQuery] bool chapNhan)
+        {
+            var kq = await _business.PhanHoiAsync(id, chapNhan);
+            if (kq.ThanhCong) return Ok(kq);
+            return NotFound(kq);
+        }
+
+        [HttpGet("theo-don/{donUngTuyenId:int}")]
+        [Authorize]
+        public async Task<IActionResult> LayTheoDon(int donUngTuyenId)
+        {
+            var ds = await _business.LayTheoDonUngTuyenAsync(donUngTuyenId);
+            return Ok(ds);
         }
     }
 }
